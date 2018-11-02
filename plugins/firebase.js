3617671ba20485d676548a5bda7cf1fd61c04113
yuki-toida <yuki.toidax@gmail.com>
2018-11-02 10:25:50 +0900

    ignore config

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,12 +3,6 @@ import firebase from 'firebase';
 if (!firebase.apps.length) {
   // Initialize Firebase
   const config = {
-    apiKey: "AIzaSyAskYpbPGlyovzshOhLifg3SmETal3yZIA",
-    authDomain: "planet-pluto-local.firebaseapp.com",
-    databaseURL: "https://planet-pluto-local.firebaseio.com",
-    projectId: "planet-pluto-local",
-    storageBucket: "planet-pluto-local.appspot.com",
-    messagingSenderId: "402255219393"
   };
   firebase.initializeApp(config);
 }
